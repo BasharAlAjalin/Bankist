9d00e70787b3a2a375bdc571f8f0de3340ab5ef3
Bashar Al-Ajalin <221134@ppu.edu.ps>
2025-10-25 10:58:54 +0200

    Building Function for display movements

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 "use strict";
 
-
 const account1 = {
   owner: "Bashar Ajalin",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, 
+  interestRate: 1.2,
   pin: 1111,
 };
 
@@ -56,4 +55,21 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  movements.forEach(function (movement, index) {
+    const type = movement > 0 ? "deposit" : "withdrawal";
 
+    const data = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      index + 1
+    } ${type}</div>
+          <div class="movements__value">${movement}</div>
+        </div>`;
+
+    containerMovements.insertAdjacentHTML("afterbegin", data);
+  });
+};
+
+displayMovements(account1.movements);
